a373a06ec7a40325cbb9dbe50afd7238339761ab
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-07-06 22:39:17 +0200

    Add Images

--- DCLearn/DCLearn/ContentView.swift
+++ DCLearn/DCLearn/ContentView.swift
@@ -6,71 +6,46 @@
 //
 
 import SwiftUI
-import CoreData
 
+
+struct ListOption: Hashable {
+    let image: String
+    let tile: String
+}
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    private let listOptions: [ListOption]  = [
+        .init(image: "house", tile: "Home"),
+        .init(image: "arrow.down.app", tile: "Downloaded"),
+        .init(image: "phone.circle", tile: "Contact"),
+        .init(image: "gear", tile: "Settings"),
+        .init(image: "info.circle", tile: "About")
+    ]
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
+        NavigationView {
+            ListView()
+            
+            MainView()
         }
-        .toolbar {
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        .frame(minWidth: 600, minHeight: 400)
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+    }
+}
+
+
+struct ListView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        Text("")
     }
 }
